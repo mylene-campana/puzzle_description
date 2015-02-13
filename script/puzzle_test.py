ee6c10ebf908263b0993788324b6385460f4bd47
Mylene Campana <mylene.campana@laas.fr>
2015-02-13 13:48:06 +0100

    Update script to Package and hpp-viewer

--- script/puzzle_test.py
+++ script/puzzle_test.py
@@ -3,29 +3,20 @@
 # RViz command : roslaunch puzzle_description puzzle.launch
 # Easy way to test planning algo (no internal DoF) on SO3 joint.
 
-from hpp_ros import ScenePublisher
-from hpp_ros import PathPlayer
-from hpp.corbaserver.puzzle_robot import Robot
+from hpp.corbaserver.puzzle import Robot
 from hpp.corbaserver import Client
+from hpp.corbaserver import ProblemSolver
 import time
-import sys
-sys.path.append('/local/mcampana/devel/hpp/src/test-hpp/script')
 
-robot = Robot ('puzzle_robot') # object5
-robot.setJointBounds('base_joint_xyz', [-0.9, 0.9, -0.9, 0.9, -1.1, 1.1])
+robot = Robot ('puzzle') # object5
+robot.setJointBounds('base_joint_xyz', [-0.9, 0.9, -0.9, 0.9, -1.2, 1.2])
 ps = ProblemSolver (robot)
 cl = robot.client
-r = Viewer (ps)
-pp = PathPlayer (cl, r)
 
-q1 = [0.0, 0.0, 0.6, 1.0, 0.0, 0.0, 0.0]; q2 = [0.0, 0.0, -0.6, 1.0, 0.0, 0.0, 0.0]
-#q1 = [0.0, 0.0, 0.8, 1.0, 0.0, 0.0, 0.0]; q2 = [0.0, 0.0, -0.8, 1.0, 0.0, 0.0, 0.0]
+q1 = [0.0, 0.0, 0.8, 1.0, 0.0, 0.0, 0.0]; q2 = [0.0, 0.0, -0.8, 1.0, 0.0, 0.0, 0.0]
 #q1 = [0.5, 0.3, 0.8, 0.7071067812, 0.0, 0.7071067812, 0.0]; q2 = [0.0, -0.6, -0.5, 1.0, 0.0, 0.0, 0.0]
-r(q1)
 
-ps.setInitialConfig (q1)
-ps.addGoalConfig (q2)
-pp = PathPlayer (cl, r)
+ps.setInitialConfig (q1); ps.addGoalConfig (q2)
 
 # Load box obstacle in HPP for collision avoidance
 cl.obstacle.loadObstacleModel('puzzle_description','decor_very_easy','')
@@ -37,26 +28,28 @@ ps.solve ()
 end=time.time()
 print "Solving time: "+str(end-begin)
 
+begin=time.time()
+ps.optimizePath (0)
+end=time.time()
+print "Solving time: "+str(end-begin)
+
+begin=time.time()
+ps.optimizePath (1)
+end=time.time()
+print "Solving time: "+str(end-begin)
+
 len(cl.problem.nodes ())
-cl.problem.pathLength(0)
+ps.pathLength(0)
+ps.pathLength(1)
+ps.pathLength(2)
 
-from trajectory_plot import xyzPlot
-pathNum = 1
-xyzPlot (cl, pathNum)
 
-# (Optional if environment ready) Display obstacle
-r.addObject('decor','decor_base') # display
-r.addObject('decor1','l_decor_one')
-r.addObject('decor2','l_decor_two')
-r.addObject('decor3','l_decor_three')
+
+from hpp.gepetto import Viewer, PathPlayer
+r = Viewer (ps)
+pp = PathPlayer (robot.client, r)
+r.loadObstacleModel ("puzzle_description","decor_very_easy","decor_very_easy")
 r(q1)
-
-# Nodes from the roadmap
-import time
-for n in  cl.problem.nodes ():
-    r (n)
-    time.sleep (.3)
-
 
 ## DEBUG commands
 cl.obstacle.getObstaclePosition('decor_base')
