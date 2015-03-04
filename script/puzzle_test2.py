4820ebc76b7c1eb6a51582226d19fa4edaded434
Mylene Campana <mylene.campana@laas.fr>
2015-03-04 17:30:36 +0100

    Update to viewer, Add script adapted to 3 freeflyer rotations, Add optimization debug info

--- script/puzzle_test2.py
+++ script/puzzle_test2.py
@@ -4,11 +4,10 @@
 # Easy way to test planning algo (no internal DoF) on SO3 joint.
 
 from hpp.gepetto import Viewer, PathPlayer
-from hpp.corbaserver.puzzle_robot import Robot
+from hpp.corbaserver.puzzle import Robot
+from hpp.corbaserver import ProblemSolver
 from hpp.corbaserver import Client
 import time
-import sys
-sys.path.append('/local/mcampana/devel/hpp/src/test-hpp/script')
 
 robot = Robot ('puzzle_robot') # object5
 robot.setJointBounds('base_joint_xyz', [-0.1, 0.1, -0.1, 0.1, -0.1, 0.1])
@@ -19,21 +18,85 @@ cl = robot.client
 pp = PathPlayer (cl, r)
 
 # Patchwork of path
-q1 = [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]; q2 = [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
-cl.problem.setInitialConfig (q1); cl.problem.addGoalConfig (q2)
-cl.problem.solve (); cl.problem.resetGoalConfigs ()
+q1 = [0, 0, 0, 1, 0, 0, 0]
+q2 = [0, 0, 0, 0.707106781, 0, 0, 0.707106781]
+# equivalent to : [0, 0, 0, -0.707106781, 0, 0, -0.707106781] q7
+ps.setInitialConfig (q1); ps.addGoalConfig (q2)
+ps.solve (); ps.resetGoalConfigs ()
 
-q3 = [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]; q4 = [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
-cl.problem.setInitialConfig (q3); cl.problem.addGoalConfig (q4)
-cl.problem.solve (); cl.problem.resetGoalConfigs ()
+q3 = [0, 0, 0, 0, 0, 0, 1]
+# equivalent to : [0, 0, 0, 0, 0, 0, -1] q8
+ps.setInitialConfig (q2); ps.addGoalConfig (q3)
+ps.solve (); ps.resetGoalConfigs ()
 
-q5 = [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]; q6 = [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0]
-cl.problem.setInitialConfig (q5); cl.problem.addGoalConfig (q6)
-cl.problem.solve ()
+q4 = [0, 0, 0, -0.707106781, 0, 0, 0.707106781]
+# equivalent to : [0, 0, 0, 0.707106781, 0, 0, -0.707106781] q9
+ps.setInitialConfig (q3); ps.addGoalConfig (q4)
+ps.solve (); ps.resetGoalConfigs ()
 
-len(cl.problem.nodes ())
-cl.problem.pathLength(0)
-cl.problem.pathLength(1)
+q5 = [0, 0, 0, -1, 0, 0, 0]
+# equivalent to : [0, 0, 0, 1, 0, 0, 0] q1, q10
+ps.setInitialConfig (q4); ps.addGoalConfig (q5)
+ps.solve (); ps.resetGoalConfigs ()
+
+"""
+q6 = [0, 0, 0, -0.707106781, 0, 0, -0.707106781]
+ps.setInitialConfig (q5); ps.addGoalConfig (q6)
+ps.solve (); ps.resetGoalConfigs ()
+
+q7 = [0, 0, 0, 0, 0, 0, -1]
+ps.setInitialConfig (q6); ps.addGoalConfig (q7)
+ps.solve (); ps.resetGoalConfigs ()
+
+q8 = [0, 0, 0, 0.707106781, 0, 0, -0.707106781]
+ps.setInitialConfig (q7); ps.addGoalConfig (q8)
+ps.solve (); ps.resetGoalConfigs ()
+
+q9 = [0, 0, 0, 1, 0, 0, 0]
+ps.setInitialConfig (q8); ps.addGoalConfig (q9)
+ps.solve (); ps.resetGoalConfigs ()
+"""
+# Only path to do a useless turn ! (Not the case if solved separately)
+# r( ps.configAtParam(7,2.17) ) = [0.0, 0.0, 0.0, -0.9999559023922103, 0.0, 0.0, -0.00939112724262876]
+
+ps.setInitialConfig (q1); ps.addGoalConfig (q5) # do a turn as expected
+ps.solve ()
+
+ps.optimizePath (4) # reduced to a point as expected
+
+len(ps.getWaypoints (0))
+ps.pathLength(5) # = 0
+
+
+##############################"
+# TEST quaternions x0 HRP2 + optimisation = meme resultat (-1) (un ou 2 tours)?
+# Oui mais il faut imposer alpha = alpha_init et ne pas MàJ H1_
+# les valeurs propres de la Hessienne sont bien >0
+q1 = [0, 0, 0, 0.9999028455952614, -0.00643910090823595, -0.012362740316661774, 1.3620998722148461e-06]
+q2 = [0, 0, 0, 0.8258496711518952, -0.2042733013060623, -0.19724860126354768, -0.4871732015735299]
+ps.setInitialConfig (q1); ps.addGoalConfig (q2)
+ps.solve (); ps.resetGoalConfigs ()
+
+q3 = [0, 0, 0, 0.6659085913630002, -0.04107471639409278, 0.06948325706470262, -0.7416540248726288]
+ps.setInitialConfig (q2); ps.addGoalConfig (q3)
+ps.solve (); ps.resetGoalConfigs ()
+
+q4 = [0, 0, 0, 0.8528688178462975, 0.1121872771500791, 0.09397345683903598, -0.5011963525414287]
+ps.setInitialConfig (q3); ps.addGoalConfig (q4)
+ps.solve (); ps.resetGoalConfigs ()
+
+q5 = [0, 0, 0, 0.9999821377631646, -0.0058327069366009565, 0.0013052524286092148, 7.07493534969223e-07]
+ps.setInitialConfig (q4); ps.addGoalConfig (q5)
+ps.solve (); ps.resetGoalConfigs ()
+
+ps.setInitialConfig (q1); ps.addGoalConfig (q5)
+ps.solve ()
+ps.pathLength(4)
+pp(4)
+
+ps.optimizePath (4)
+ps.pathLength(5)
+pp(5)
 
 ## DEBUG commands
 begin=time.time(); end=time.time(); print "Solving time: "+str(end-begin)
--- script/puzzle_test_ROTS.py
+++ script/puzzle_test_ROTS.py
@@ -10,14 +10,16 @@ import time
 import numpy as np
 
 robot = Robot ('puzzle') # object5
-#robot.setJointBounds('base_joint_xyz', [-0.9, 0.9, -0.9, 0.9, -1.2, 1.2])
-robot.setJointBounds('base_joint_xyz', [-0.6, 0.6, -0.6, 0.6, -0.3, 1.0])
+robot.setJointBounds('base_joint_xyz', [-0.9, 0.9, -0.9, 0.9, -1.2, 1.2])
+#robot.setJointBounds('base_joint_xyz', [-0.6, 0.6, -0.6, 0.6, -0.3, 1.0])
 ps = ProblemSolver (robot)
 cl = robot.client
+robot.getJointNames ()
+robot.getConfigSize ()
 
-#q1 = [0.0, 0.0, 0.8, 1.0, 0.0, 0.0, 0.0]; q2 = [0.0, 0.0, -0.8, 1.0, 0.0, 0.0, 0.0]
-q1 = [0.0, 0.0, 0.8, 1.0, 0.0, 0.0, 0.0]; q2 = [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0] # DEBUG
-#q1 = [0.5, 0.3, 0.8, 0.7071067812, 0.0, 0.7071067812, 0.0]; q2 = [0.0, -0.6, -0.5, 1.0, 0.0, 0.0, 0.0]
+# Configs : [x, y, z, qx1, qx2, qy1, qy2, qz1, qz2]
+q1 = [0, 0, 0.8, 1, 0, 1, 0, 1, 0]; q2 = [0, 0, -0.8, 1, 0, 1, 0, 1, 0]
+#q1 = [0, 0, 0.8, 1, 0, 1, 0, 1, 0]; q2 = [0, 0, 0, 1, 0, 1, 0, 1, 0] # DEBUG
 
 from hpp.gepetto import Viewer, PathPlayer
 r = Viewer (ps)
@@ -41,35 +43,18 @@ begin=time.time()
 ps.optimizePath (0)
 end=time.time()
 print "Optim time: "+str(end-begin)
+cl.problem.getIterationNumber ()
 
 begin=time.time()
 ps.optimizePath (1)
 end=time.time()
 print "Optim time: "+str(end-begin)
 
-len(cl.problem.nodes ())
 len(ps.getWaypoints (0))
+cl.problem.getIterationNumber ()
 ps.pathLength(0)
 ps.pathLength(1)
 ps.pathLength(2)
-
-qconstr0 = [0.0136688,0.409213,0.504901,-0.622972,-0.551659,-0.113669,-0.542824]
-qconstr1 = [-0.000512377,0.397699,0.494112,-0.612723,-0.545536,-0.144922,-0.553135]
-qconstr2 = [-0.393372,0.145117,0.404677,0.58047,0.129693,-0.801317,-0.0642316]
-qconstr3 = [-0.357582,0.131914,0.367858,0.648702,0.121214,-0.748926,-0.0600321]
-
-qconstr4 = [-0.407994,0.150511,0.419719,0.551315,0.132881,-0.821014,-0.0658105] # COLL
-qconstr5 = [-0.407994,0.150511,0.419719,0.551315,0.132881,-0.821014,-0.0658105] # =4
-
-qconstr6 = [-0.408971,0.150871,0.420724,0.549341,0.133089,-0.822294,-0.0659131]
-
-
-qconstr1 = [-0.412662,0.160256,0.434482,0.505776,0.116219,-0.850206,-0.0885047]
-qconstr2 = [-0.408678,0.150763,0.420422,0.549934,0.133026,-0.82191,-0.0658823] # COLL
-
-qconstrx0 = np.array(ps.getWaypoints (0)[3])*(1-0.029617) + 0.029617*np.array(ps.getWaypoints (0)[4])
-
-r([-0.408678,  0.150763,  0.420422,  0.53857 ,  0.131591, -0.813046, -0.065171])
 
 ## DEBUG commands
 cl.obstacle.getObstaclePosition('decor_base')
